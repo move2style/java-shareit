5b7797883aa12aa711c254ef5dfbc3bc51c6633b
glazu <glazunovnm@yandex.ru>
2025-10-02 20:11:46 +0300

    тестовый коммит

--- server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -21,7 +21,6 @@ import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.service.UserService;
 
 import java.time.LocalDateTime;
-import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
